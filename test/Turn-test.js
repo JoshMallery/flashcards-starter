e3a525cf5469165e14b6896d3071c7c6aeeb113d
Josh Mallery <josh.mallery@gmail.com>
2022-03-21 23:22:57 -0600

    Refactor Turn-test.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,8 +5,6 @@ const Card = require('../src/Card')
 const Turn = require('../src/Turn');
 
 describe('Turn Tests Suite', () => {
-  let card;
-  let turn;
 
   beforeEach(() => {
     card = new Card(1, 'What is Robbie\'s favorite animal', ['right answer', 'pug', 'capybara'], 'right guess');
@@ -14,56 +12,38 @@ describe('Turn Tests Suite', () => {
   });
 
   it('should be a function', () =>{
-    // const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', () =>{
-    // const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
   it('should be instantiated with two arguments, a string and a Card object', () => {
-    // const card = new Card();
-    // const turn = new Turn('guess',card);
     expect(turn.guess).to.equal('right guess');
     expect(turn.card).to.equal(card);
   });
 
   it('should have a method that returns the guess', () => {
-    // const turn = new Turn('guess');
     expect(turn.returnGuess()).to.equal('right guess');
   });
 
   it('should have a method that returns the card', () => {
-    // const card = new Card();
-    // const turn = new Turn('guess',card);
-    expect(turn.returnCard()).to.deep.equal(card); //card.id
+    expect(turn.returnCard()).to.deep.equal(card);
   });
 
   it('should evaluate if the answer is correct or not', () => {
-    // const card = new Card(); // need full card info
-    //
-    // const turnRight = new Turn('right guess',card);
     const turnWrong = new Turn('wrong guess',card);
 
-    expect(turn.evaluateGuess()).to.equal(true); //check and see if there is a special boolean for this
-    expect(turnWrong.evaluateGuess()).to.equal(false);
-
-    //need to test sad cases
-    // const card = new Card(); // need full card info
+    expect(turn.evaluateGuess()).to.be.true;
+    expect(turnWrong.evaluateGuess()).to.be.false;
   });
 
   it('should give feedback on right or wrong answers', () => {
-    // const card = new Card() //need full card info
-    //
-    // const turnRight = new Turn('right guess',card);
     const turnWrong = new Turn('wrong guess',card);
 
     expect(turn.giveFeedback()).to.equal('correct!');
     expect(turnWrong.giveFeedback()).to.equal('incorrect!');
-
-
   });
 
 })
